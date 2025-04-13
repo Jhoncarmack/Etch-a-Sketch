4b036ad3b965c560419bcd6af5c31c29e7022cdd
Jhoncarmack <165399569+Jhoncarmack@users.noreply.github.com>
2025-04-13 17:41:29 +0900

    feat: Flexbox로 동적 2D 그리드 레이아웃 생성
     - JavaScript를 이용해  사용자 입력 그리드 생성
     - 버튼 요소를 통해 입력값을 받아 동적 그리드 생성
     - container 안에 Flexbox로 그리드 정렬
     - container 및 div 스타일 적용으로 N x N 정렬 유지

--- practice.js
+++ practice.js
@@ -1,7 +1,22 @@
-// const div = document.querySelector("div");
-// const btn = document.createElement("button");
-// div.appendChild(btn);
+const btn = document.createElement("button");
+const body = document.querySelector("body");
+body.appendChild(btn);
+btn.className = "button";
+btn.style.height = "auto";
+btn.textContent = "button";
+const container = document.querySelector(".container");
+btn.addEventListener("click", () => {
+  container.textContent = "";
+  let select = Number(prompt("수 입력: "));
 
-// btn.addEventListener("click", () => {
-//   div.classList.toggle("color");
-// });
+  for (let i = 1; i <= select; i++) {
+    for (let j = 1; j <= select; j++) {
+      const divP = document.createElement("div");
+      container.appendChild(divP);
+
+      divP.style.width = `calc(100%/${select})`;
+      divP.style.height = `calc(100%/${select})`;
+      divP.className = "box";
+    }
+  }
+});
