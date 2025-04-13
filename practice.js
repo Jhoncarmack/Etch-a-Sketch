5344eb3425da395a7c3981eec8c7777e86d97f84
Jhoncarmack <165399569+Jhoncarmack@users.noreply.github.com>
2025-04-13 20:58:10 +0900

    feat: mouseenter 이벤트로 호버 효과 구현
     - 각 박스 div에 마우스를 올리면 .color 클래스가 추가되도록 구현

--- practice.js
+++ practice.js
@@ -5,6 +5,7 @@ btn.className = "button";
 btn.style.height = "auto";
 btn.textContent = "button";
 const container = document.querySelector(".container");
+
 btn.addEventListener("click", () => {
   container.textContent = "";
   let select = Number(prompt("수 입력: "));
@@ -13,10 +14,13 @@ btn.addEventListener("click", () => {
     for (let j = 1; j <= select; j++) {
       const divP = document.createElement("div");
       container.appendChild(divP);
-
       divP.style.width = `calc(100%/${select})`;
       divP.style.height = `calc(100%/${select})`;
-      divP.className = "box";
+      divP.classList.add("box");
+
+      divP.addEventListener("mouseenter", () => {
+        divP.classList.add("color");
+      });
     }
   }
 });
